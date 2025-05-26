64c1e69cbee6d985631960f95b73a20e505a1af9
unknown <carlosroque117@gmail.com>
2025-05-26 16:18:09 +0800

    Created a class and using init method then turn the main functions into method.

--- quiz_program_taker_oop.py
+++ quiz_program_taker_oop.py
@@ -1,33 +1,39 @@
-#Import Library of Choice
+
 import random
 
-#Read questions from the saved file
-def load_questions_from_file(filename="quiz_questions.txt"):
-    questions = []
-    try:
-        with open(filename, "r") as file:
-            content = file.read().strip().split("---n")
-            for block in content:
-                lines = block.strip().split("\n")
-                if len(lines)>= 6:
-                    question_text = lines[0].replace("Question: ", "")
-                    choices = {
-                        'a': lines [1][3:].strip(),
-                        'b': lines [2][3:].strip(),
-                        'c': lines [3][3:].strip(),
-                        'd': lines [4][3:].strip()
-                    }
-                    correct_choice = lines[5].replace("Correct Answer is: ", "").strip()
-                    questions.append({
-                        "question": question_text,
-                        "options": choices,
-                        "answer": correct_choice
-                    })
-        return questions
-    except FileNotFoundError:
-        print("No quiz questions found. Make sure to run the quz_maker_game before running the program.")    
-        return []
-#Ask a question to the user and check the answer
+
+class QuizGame: 
+    def __init__(self, filename= "quiz_questions.txt"):
+        self.filename = filename
+        self.questions = self.load_questions_from_file()
+        self.score = 0 
+        
+    def load_questions_from_file(self, filename="quiz_questions.txt"):
+        questions = []
+        try:
+            with open(filename, "r") as file:
+                content = file.read().strip().split("---n")
+                for block in content:
+                    lines = block.strip().split("\n")
+                    if len(lines)>= 6:
+                        question_text = lines[0].replace("Question: ", "")
+                        choices = {
+                            'a': lines [1][3:].strip(),
+                            'b': lines [2][3:].strip(),
+                            'c': lines [3][3:].strip(),
+                            'd': lines [4][3:].strip()
+                        }
+                        correct_choice = lines[5].replace("Correct Answer is: ", "").strip()
+                        questions.append({
+                            "question": question_text,
+                            "options": choices,
+                            "answer": correct_choice
+                        })
+            return questions
+        except FileNotFoundError:
+            print("No quiz questions found. Make sure to run the quz_maker_game before running the program.")    
+            return []
+
 def ask_question(question_data):
     print("\n" + question_data["question"])
     for key, option in question_data["options"].items():
@@ -45,7 +51,7 @@ def ask_question(question_data):
         print(f"Your answer is Wrong! The correct answer is: {question_data['answer']}){correct_option}")
         return False 
     
-#Run the quiz game
+
 def run_quiz():
     print("Welcome to the quiz game!")
     saved_questions = load_questions_from_file()
